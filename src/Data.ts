75d9bbe63f95d149870feb39c73fda6d41382682
Pavlos Vinieratos <pvinis@gmail.com>
2020-11-28 12:50:34 +0000

    add data

---
+++ src/Data.ts
@@ -0,0 +1,13 @@
+import {DateTime} from "luxon"
+
+
+type Entry = {
+	date: DateTime
+	text: string
+}
+
+
+export const Data: Entry[] = [{
+	 date:   DateTime.local(1988,2,25).setZone("Europe/Athens", {keepLocalTime: true}),
+	text: "birth"
+}]
