323e0722c32b5ae41e739ece1dc7c34b6d7cb199
Pavlos Vinieratos <pvinis@gmail.com>
2020-11-28 12:51:11 +0000

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {range}  from "ramda"
 
-function App() {
+import { Data } from "./Data"
+
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+	{[range(20, 40).map(index => {
+		<p>wow{index}</p>
+	})]}
+	<li>
+	{Data.map(entry=>{
+			return (
+			<ul>- {entry.text}: {entry.date.toString()}</ul>
+			)
+		})}
+	</li>
     </div>
   );
 }
-
-export default App;
