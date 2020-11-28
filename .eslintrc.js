d06906f89c9f98e4f46b3d941c4155909b5c1863
Pavlos Vinieratos <pvinis@gmail.com>
2020-11-28 14:20:09 +0000

    aha

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,37 @@
+const ERR = 'error'
 
-module.exports={
+
+module.exports = {
 	root: true,
-    extends: [
-        "eslint:recommended"
-    ]
-    // "extends": [
-    //   "react-app",
-    //   "react-app/jest"
-    // ]
+	parser: '@typescript-eslint/parser',
+	env: {
+		browser: true,
+		es6: true,
+		node: true,
+	},
+	plugins: ['@typescript-eslint', 'react'],
+	extends: [
+		'eslint:recommended',
+		//  "react-app",
+		//   "react-app/jest"
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	rules: {
+		indent: [ERR, 'tab'],
+		semi: [ERR, 'never'],
+		'comma-dangle': [ERR, 'always-multiline'],
+		quotes: [ERR, 'single'],
+		'jsx-quotes': [ERR, 'prefer-single'],
+		'no-multiple-empty-lines': [ERR, { max: 2 }],
+		'object-curly-spacing': [ERR, 'always'],
+		'key-spacing': [ERR],
+		'array-bracket-spacing': [ERR],
+		'comma-spacing': [ERR],
+	},
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
-import {range}  from "ramda"
+import { range } from 'ramda'
 
-import { Data } from "./Data"
+import { Data } from './Data'
 
 
 export const App = () => {
-  return (
-    <div>
-	{[range(20, 40).map(index => {
-		<p key={`${index}`}>wow{index}</p>
-	})]}
-	<li>
-	{Data.map(entry=>{
-			return (
-			<ul key={entry.text}>- {entry.text}: {entry.date.toString()}</ul>
-			)
-		})}
-	</li>
-    </div>
-  );
+	return (
+		<div>
+			{[
+				range(20, 40).map((index) => {
+					<p key={`${index}`}>wow{index}</p>
+				}),
+			]}
+			<li>
+				{Data.map((entry) => {
+					return (
+						<ul key={entry.text}>
+							- {entry.text}: {entry.date.toString()}
+						</ul>
+					)
+				})}
+			</li>
+		</div>
+	)
 }
