008f1f203ad38213b88c1ab0c299acc195d51b04
Pavlos Vinieratos <pvinis@gmail.com>
2020-11-30 16:22:00 +0000

    some tags

--- src/Data.ts
+++ src/Data.ts
@@ -22,10 +22,12 @@ export const Data: Entry[] = [
 	{
 		date: DateTime.local(1988, 2, 25).setZone('Europe/Athens', { keepLocalTime: true }),
 		text: 'birth',
+		tags: ['general'],
 	},
 	...range(1, 33).map(year => ({
 		date: DateTime.local(1988+year, 2, 25),
 		text: `${year} birthday`,
+		tags: ['general'],
 	})),
 	{
 		date: now,
@@ -34,36 +36,45 @@ export const Data: Entry[] = [
 	{
 		interval: Interval.fromDateTimes(DateTime.local(2005, 9, 4), DateTime.local(2010, 7, 10)),
 		text: 'di uoa',
+		tags: ['studies'],
 	},
 	{
 		interval: Interval.fromDateTimes(DateTime.local(2010, 9, 4), DateTime.local(2015, 7, 10)),
 		text: 'di uoa master',
+		tags: ['studies'],
 	},
 	{
 		interval: Interval.fromDateTimes(DateTime.local(1988, 2, 25), DateTime.local(2014, 3, 3)),
 		text: 'live in Athens',
+		tags: ['living-place'],
 	},
 	{
 		interval: Interval.fromDateTimes(DateTime.local(2014, 3, 3), DateTime.local(2016, 11, 3)),
 		text: 'live in Vienna',
+		tags: ['living-place'],
 	},
 	{
 		interval: Interval.fromDateTimes(DateTime.local(2016, 11, 3), DateTime.local(2018, 11, 3)),
 		text: 'live in Amsterdam',
+		tags: ['living-place'],
 	},
 	{
 		interval: Interval.fromDateTimes(DateTime.local(2018, 11, 3), DateTime.local(2020, 11, 3)),
 		text: 'live in Rotterdam',
-	},
-	{
-		date: DateTime.local(1988, 2, 25).plus({ years: 18 }),
-		text: 'turned 18',
+		tags: ['living-place'],
 	},
 	{
 		interval: Interval.fromDateTimes(DateTime.local(2020, 11, 3), now),
 		text: 'live in London',
+		tags: ['living-place'],
 	},
 ]
 
 
 /// have tags as buttons that only show these events. like a filter. only "living-city" for example
+
+///  nicer tooltip on hover
+
+//// make mobile work with animation
+
+/// make some private ones, that can be accessed with a password or something? also in a file thats encrypted on gh
